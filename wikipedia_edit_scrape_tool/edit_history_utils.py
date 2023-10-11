f3e29ca43a8036eb81c1321843890972c25489f0
smfsamir <f.samir@mail.utoronto.ca>
2023-10-10 19:16:00 -0700

    add edit history utils

---
+++ wikipedia_edit_scrape_tool/edit_history_utils.py
@@ -0,0 +1,15 @@
+"""Utility functions for manipulating the edit history object
+(for a given person)
+"""
+from typing import Any, Dict, List, Tuple
+from .scrape_edit_history import WikipageSnapshot
+
+def get_last_snapshot(edit_history_obj: Dict[str, Any], 
+                      lang_wiki: str) -> WikipageSnapshot:
+    """_summary_
+
+    Args:
+        edit_history_obj (_type_): Get the current page content from the edit history object.
+    """
+    final_snapshot = edit_history_obj['edit_history_snapshots_all_languages'][lang_wiki][-1]
+    return final_snapshot
