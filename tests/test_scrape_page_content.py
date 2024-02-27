df83292dfb3ae46e14abb400974c6927ce471512
Farhan Samir <f.samir@mail.utoronto.ca>
2024-02-27 15:22:34 -0800

    update

--- tests/test_scrape_page_content.py
+++ tests/test_scrape_page_content.py
@@ -1,7 +1,8 @@
 import ipdb
 import pytest
 
-from wikipedia_edit_scrape_tool.scrape_page_content import get_text, clean_paragraph
+from wikipedia_edit_scrape_tool.scrape_page_content import get_text, clean_paragraph, Paragraph
+
 # from wikipedia_edit_scrape_tool.scrape_page_content import get_multilingual_wikilinks_mediawiki
 
 def test_paragraphs_delineated_en():
@@ -26,3 +27,14 @@ def test_clean_paragraph():
     assert "[1][2]" not in cleaned_summary_paragraph
     # assert that there are no \n, unless it's for starting a new paragraph (i.e., \n\n at the end of a paragraph)
     assert "\n" not in cleaned_summary_paragraph[:-2]
+
+# TODO: use this to merge block quotes with the paragraph that they are part of
+def test_alan_turing_paragraphs():
+    person_id = 'Alan_Turing'
+    en_link = f"https://en.wikipedia.org/wiki/{person_id}"
+    content = get_text(en_link, 'enwiki')
+    # get the text of the paragraph that says 'Alexander wrote of Turing's contribution:'
+    turing_cont_paragraph = [p for p in content if isinstance(p, Paragraph) and 'Alexander wrote of Turing\'s contribution:' in p.clean_text][0]
+
+    print(content[2])
+    ipdb.set_trace()
